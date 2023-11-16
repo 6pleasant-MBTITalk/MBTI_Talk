b982a0825c5f772e69c17b1570fe01d30477d9eb
limduhyoung <limduh2@naver.com>
2023-11-16 09:57:26 +0900

    수정~

--- app/src/main/java/com/example/mbti_talk/SignUpActivity.kt
+++ app/src/main/java/com/example/mbti_talk/SignUpActivity.kt
@@ -123,7 +123,7 @@ class SignUpActivity : AppCompatActivity() {
 
             }
         }
-        //
+
         binding.SignUpBtnSignUp.setOnClickListener {
 
             val builder = AlertDialog.Builder(this)
